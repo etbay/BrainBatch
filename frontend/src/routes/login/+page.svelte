97faf793e954861038068252c126923c3f22e041
timowerronen <timowerronen@gmail.com>
2025-09-18 22:51:05 -0700

    start a login page (basic and non-functional at this point)

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,71 @@
+<div class="login-container">
+    <h1>Log in to BrainBatch</h1>
+    <p>Don't have an account yet? <a href="/createaccount">Create one!</a></p>
+    <form>
+        <div class="form-field">
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username" required>
+        </div>
+        <div class="form-field">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <input type="submit" value="Log in">
+    </form>
+</div>
+
+<style>
+    .login-container {
+        background-color: #fff;
+        width: 600px;
+        max-width: 100%;
+        margin: 1rem auto;
+        padding: 2rem;
+        border: 1px solid #808080;
+        border-radius: 1rem;
+    }
+
+    .login-container h1, .login-container p {
+        text-align: center;
+        margin: 1rem;
+    }
+
+    form {
+        margin: 2rem auto;
+        max-width: 400px;
+    }
+
+    .form-field {
+        margin-bottom: 1rem;
+    }
+
+    .form-field label {
+        display: block;
+        margin-bottom: 0.5rem;
+    }
+
+    .form-field input {
+        display: block;
+        padding: 0.75rem;
+        width: 100%;
+        border: 1px solid #808080;
+        border-radius: 6px;
+    }
+
+    form input[type=submit] {
+        display: block;
+        margin: 1rem auto;
+        background-color: #ff3e00;
+        color: #fff;
+        font-size: 1.25em;
+        font-weight: bold;
+        padding: 0.5em 1em;
+        border: none;
+        border-radius: 6px;
+    }
+
+    form input[type=submit]:hover {
+        cursor: pointer;
+        box-shadow: #912501 3px 3px;
+    }
+</style>

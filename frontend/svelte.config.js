21dfa1d2821a79d99905be52b3a11fb6d4455a70
timowerronen <timowerronen@gmail.com>
2025-12-03 17:13:06 -0800

    modify adapter config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+// Deploy the app by running `npm run build`. Then, start the server with `node build`.
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
